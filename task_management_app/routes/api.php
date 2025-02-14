7a5172e0b482fd2814519d4361464e8b69a4775b
Teodora Krkic <teodora.krkic03@gmail.com>
2025-02-14 18:43:21 +0100

    dodate nove rute i ogranicenja

--- task_management_app/routes/api.php
+++ task_management_app/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskListController;
 use App\Http\Controllers\UserController;
@@ -10,22 +11,11 @@ use App\Models\Status;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
+//prikazivanje korisnika
 Route::get('users', [UserController::class,'index']);
 Route::get('users/{id}', [UserController::class,'show']);
-Route::post('users',[UserController::class,'store']);
-Route::put('users/{id}',[UserController::class,'update']);
-Route::delete('users/{id}',[UserController::class,'destroy']);
 
-Route::resource('tasks',TaskController::class)->only(['index','show']);
-
-Route::get('task_lists',[TaskListController::class,'index']);
-Route::get('task_lists/{id}',[TaskListController::class,'show']);
-
+//prikaz dostupnih kategorija
 Route::get('categories', function(){
     $categories = Category::all();
     return CategoryResource::collection($categories);
@@ -38,12 +28,37 @@ Route::get('categories/{id}', function(string $id){
     return new CategoryResource($category);
 })->whereNumber('id');
 
+//prikaz dostupnih statusa
 Route::get('statuses', function(){
     $statuses = Status::all();
     return response()->json($statuses);
 });
 
+//prikaz dostupnih prioriteta
 Route::get('priorities', function(){
     $priorities = Priority::all();
     return response()->json($priorities);
+});
+
+
+Route::post('register',[AuthController::class,'register']);
+Route::post('login',[AuthController::class,'login']);
+
+Route::group(['middleware'=> ['auth:sanctum']], function(){
+    //rute za azuriranje i brisanje korisnika
+    Route::put('users/{id}',[UserController::class,'update']);
+    Route::delete('users/{id}',[UserController::class,'destroy']);
+
+    //rute za rad za zadacima
+    Route::resource('tasks',TaskController::class)->only(['index','show','store','update','destroy']);
+
+    //rute za rad sa listama zadataka
+    Route::get('task_lists',[TaskListController::class,'index']);
+    Route::get('task_lists/{id}',[TaskListController::class,'show']);
+    Route::post('task_lists',[TaskListController::class,'store']);
+    Route::put('task_lists/{id}',[TaskListController::class,'update']);
+    Route::delete('task_lists/{id}',[TaskListController::class,'destroy']);
+
+    //logout
+    Route::post('logout', [AuthController::class,'logout']);
 });
