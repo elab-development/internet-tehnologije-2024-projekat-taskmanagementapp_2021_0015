18220975cd742751dd0d41148f11832eb43f7124
Teodora Krkic <teodora.krkic03@gmail.com>
2025-07-24 15:28:02 +0200

    kreirane rute za prikaz zadataka i listi

--- tma_frontend/src/App.js
+++ tma_frontend/src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import {zadaci, kategorije, liste, redosled, korisnici} from './Data';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
+
+const status = ['Nije zapocet', 'U toku', 'Zavrseno'];
+const prioritet = ['Visok', 'Srednji', 'Nizak'];
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+     <div>
+      <Routes>
+        <Route 
+          path = '/tasks'
+          element = {
+            <>
+              <div className='header'>
+                <NavBar />
+              </div>
+              <div className='contents'>
+                <div className='main-container'>
+                  
+                </div>
+
+              </div>
+            </>
+          }
+        />
+        <Route
+          path = '/lists'
+          element = {
+            <>
+              <div className='header'>
+                <NavBar />
+              </div>
+              <div className='lists-page'>
+
+              </div>
+            </>
+          }
+        />
+
+        
+      </Routes>
+     </div>
+    </BrowserRouter>
   );
 }
 
