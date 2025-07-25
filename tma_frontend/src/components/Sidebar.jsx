149cf7eee97622af2f66c4d645294e63d0bf4837
Teodora Krkic <teodora.krkic03@gmail.com>
2025-07-25 12:04:10 +0200

    kreirana Sidebar komponenta

---
+++ tma_frontend/src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Search from './Search'
+
+const Sidebar = ({}) => {
+  return (
+    <div className='sidebar'>
+        <div className='search-and-filter'>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
