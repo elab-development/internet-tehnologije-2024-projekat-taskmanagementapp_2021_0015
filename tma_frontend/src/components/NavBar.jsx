e3467a82301ef59b134150d9da4ec8d43628b7c0
Teodora Krkic <teodora.krkic03@gmail.com>
2025-07-28 16:13:50 +0200

    minimalno izmenjeno stilizovanje NavBar-a

--- tma_frontend/src/components/NavBar.jsx
+++ tma_frontend/src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import '../css/NavBar.css';
 import {useAuth} from '../AuthContext';
+import miniLogo from '../notes-icon.png';
 
 const NavBar = ({type}) => {
   const {logout} = useAuth();
@@ -15,21 +16,27 @@ const NavBar = ({type}) => {
   if(type==='normal')
     return (
       <div className='NavBar'>
+        <div className='app-title'>
+          <img src={miniLogo} alt='Logo' width={35} height={35}/>
           <Link to='/tasks'><h1>TaskMaster</h1></Link>
-          <div className='pages'>
-              <Link to='/lists'>Lists</Link>
-              <button className='logout-btn' onClick={handleLogout}>Logout</button>
-          </div>
+        </div>
+        <div className='pages'>
+          <Link to='/lists'>Lists</Link>
+          <button className='logout-btn' onClick={handleLogout}>Logout</button>
+        </div>
       </div>
     )
 
   if(type==='auth')
     return(
       <div className='NavBar'>
+        <div className='app-title'>
+          <img src={miniLogo} alt='Logo' width={35} height={35}/>
           <Link to='/tasks'><h1>TaskMaster</h1></Link>
-          <div className='pages'>
+        </div>
+        <div className='pages'>
               <Link to='/register'>Register</Link>
-          </div>
+        </div>
       </div>
     )
 }
